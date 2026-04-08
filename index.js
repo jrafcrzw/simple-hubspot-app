72cc28c4e5713657eaad02d57c6be21d696f1580
jrafcrzw <dev@jrafcrz.work>
2026-04-08 14:29:08 -0600

    feat(api): make first successful API call and save response to response.txt

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
+require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
 
 const app = express();
+
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+
+app.get('/', async (req, res) => {
+    const companies = 'https://api.hubspot.com/crm/v3/objects/contacts';
+    const headers = {
+        Authorization: `Bearer ${ACCESS_TOKEN}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const response = await axios.get(companies, { headers });
+        res.json(response.data.results);    
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
